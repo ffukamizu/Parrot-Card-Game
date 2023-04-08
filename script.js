f584df35f80dd03cc4c0db5c474b808e7aac8207
ffukamizu <f.fukamizu@live.com>
2023-04-08 04:50:40 -0300

    game's main functionality completed and some bug fixes

--- script.js
+++ script.js
@@ -3,27 +3,11 @@ let deckSize = 0;
 do {
   deckSize = prompt("Insira a quantidade de cartas que deseja jogar, entre 4 e 14 (Números pares apenas)");
 } while (deckSize < 4 || deckSize > 14 || deckSize % 2 != 0);
-//creates an array with all the possible faces
-const deckStack = ["parrot-bob", "parrot-explody", "parrot-fiesta", "parrot-metal", "parrot-captain", "parrot-triplet", "parrot-unicorn"];
-//Fisher-Yates random sorting algorithm applies randomness to deckStack array
-for (let i = deckStack.length - 1; i > 0; i--) {
-  let j = Math.floor(Math.random() * (i + 1));
-  let k = deckStack[i];
-  deckStack[i] = deckStack[j];
-  deckStack[j] = k;
-}
-//deckStack array spliced to form a new array with desired deck size
-const deckPlayed = deckStack.splice(0, deckSize / 2);
-//display all selected cards on screen
-for (let i = 0; i < deckPlayed.length; i++) {
-  let element = document.getElementsByClassName(deckPlayed[i]);
-  for (let i = 0; i < element.length; i++) {
-    element[i].classList.add("card-played");
-  }
-}
-//Fischer-Yates shuffle function
+//Fischer-Yates random sorting algorithm
 function shuffle(array) {
-  let i = array.length, k, j;
+  let i = array.length,
+    k,
+    j;
   while (0 !== i) {
     j = Math.floor(Math.random() * i);
     i -= 1;
@@ -33,10 +17,62 @@ function shuffle(array) {
   }
   return array;
 }
+//creates an array with all the possible faces
+const deckStack = ["parrot-bob", "parrot-explody", "parrot-fiesta", "parrot-metal", "parrot-captain", "parrot-triplet", "parrot-unicorn"];
+shuffle(deckStack);
+//deckStack array spliced to form a new array with desired deck size
+const deckPlayed = deckStack.splice(0, deckSize / 2);
+//display all selected cards on screen
+for (let i = 0; i < deckPlayed.length; i++) {
+  const element = document.getElementsByClassName(deckPlayed[i]);
+  for (let i = 0; i < element.length; i++) {
+    element[i].classList.add("card-played");
+  }
+}
 //shuffles all cards currently displayed
 const gameContainer = document.querySelector(".game-container");
 const children = [...gameContainer.children];
 shuffle(children);
 for (const child of children) {
   gameContainer.appendChild(child);
+}
+//
+function cardDisplay(element) {
+  element.childNodes[1].classList.add("card-front-flip");
+  element.childNodes[3].classList.add("card-back-flip");
+}
+
+let cardRevealed = false;
+let lastElement = null;
+let lastElementClass = null;
+
+function cardSelector(element, elementClass) {
+  if (cardRevealed === false) {
+    cardDisplay(element);
+    cardRevealed = true;
+    lastElement = element;
+    lastElementClass = elementClass;
+  } else if (cardRevealed === true && elementClass === lastElementClass && element !== lastElement) {
+    cardDisplay(element);
+    cardRevealed = false;
+  } else if (cardRevealed === true && elementClass !== lastElementClass && element !== lastElement) {
+    cardDisplay(element);
+    cardRevealed = false;
+    lastElementClass = null;
+    document.addEventListener("click", handler, true);
+    function handler(e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    function cardHideFirst() {
+      element.childNodes[1].classList.remove("card-front-flip");
+      element.childNodes[3].classList.remove("card-back-flip");
+      lastElement.childNodes[1].classList.remove("card-front-flip");
+      lastElement.childNodes[3].classList.remove("card-back-flip");
+      document.removeEventListener("click", handler, true);
+    }
+    setTimeout(cardHideFirst, 1000);
+  } else {
+    null;
+  }
 }
