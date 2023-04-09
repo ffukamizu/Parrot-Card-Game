c246ddc891b36c22ea4cbead9c1c0777ae00702b
ffukamizu <f.fukamizu@live.com>
2023-04-09 05:16:16 -0300

    bug fix

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let chronometer = null;
 let cardCounter = 0;
 let gameStop = 0;
 let timer = 0;
-
 //asks the user for the deck size to be played
 function gameLoad() {
   deckSize = 0;
@@ -22,6 +21,7 @@ function gameLoad() {
     let i = array.length,
       k,
       j;
+
     while (0 !== i) {
       j = Math.floor(Math.random() * i);
       i -= 1;
@@ -33,6 +33,7 @@ function gameLoad() {
   }
   //creates an array with all the possible faces
   const deckStack = ["parrot-bob", "parrot-explody", "parrot-fiesta", "parrot-metal", "parrot-captain", "parrot-triplet", "parrot-unicorn"];
+
   shuffle(deckStack);
   //deckStack array spliced to form a new array with desired deck size
   deckPlayed = deckStack.splice(0, deckSize / 2);
@@ -47,7 +48,9 @@ function gameLoad() {
   //shuffles all cards currently displayed
   const gameContainer = document.querySelector(".game-container");
   const children = [...gameContainer.children];
+
   shuffle(children);
+
   for (const child of children) {
     gameContainer.appendChild(child);
   }
@@ -65,7 +68,7 @@ function cardDisplay(element) {
   element.childNodes[1].classList.add("card-front-flip");
   element.childNodes[3].classList.add("card-back-flip");
 }
-//displays end game mensage
+//displays end game mensage and reset prompt
 function gameEnd() {
   alert(`Você ganhou em ${cardCounter} jogadas! A duração do jogo foi de ${timer} segundos!`);
 
@@ -83,6 +86,7 @@ function gameEnd() {
     }
 
     document.getElementById("timer").innerHTML = "0";
+
     clearInterval(chronometer);
 
     cardRevealed = false;
@@ -97,48 +101,50 @@ function gameEnd() {
     clearInterval(chronometer);
   }
 }
-//core gameplay function
+//core gameplay function, handles card flip and unflip logic
 function cardSelector(element, elementClass) {
-  //will reveal a card if no other is flipped
   if (cardRevealed === false) {
     cardDisplay(element);
+
     cardRevealed = true;
     lastElement = element;
     lastElementClass = elementClass;
     cardCounter++;
-    //reveals another card if its the same face
   } else if (cardRevealed === true && elementClass === lastElementClass && element !== lastElement) {
     cardDisplay(element);
+
     cardRevealed = false;
     cardCounter++;
     gameStop++;
-    
+
     if (gameStop == deckPlayed.length) {
       setTimeout(gameEnd, 1000);
     }
-    //flips both cards back if they are the wrong pair
   } else if (cardRevealed === true && elementClass !== lastElementClass && element !== lastElement) {
     cardDisplay(element);
+
     cardRevealed = false;
     lastElementClass = null;
     cardCounter++;
-    //disables click event
+
     document.addEventListener("click", disableClick, true);
-    
+
     function disableClick(e) {
       e.stopPropagation();
       e.preventDefault();
     }
-    //unflip a card when called
+
     function cardHide() {
       element.childNodes[1].classList.remove("card-front-flip");
       element.childNodes[3].classList.remove("card-back-flip");
       lastElement.childNodes[1].classList.remove("card-front-flip");
       lastElement.childNodes[3].classList.remove("card-back-flip");
     }
+
     function restoreClick() {
       document.removeEventListener("click", disableClick, true);
     }
+
     setTimeout(cardHide, 1000);
     setTimeout(restoreClick, 1000);
   } else {
